d6a43216f761f3e96564a7cdf015616256ff2931
Joan Pablo <joanpagarf09mail.com>
2025-01-02 02:07:24 -0600

    Actualización de vite.config

--- hecho-en-casa-GH/vite.config.js
+++ hecho-en-casa-GH/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/inicio/menu.css', 'resources/css/inicio/pie.css', 'resources/css/inicio/cuerpo.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        outDir: 'public/build', // Salida de los archivos compilados
+    },
 });
