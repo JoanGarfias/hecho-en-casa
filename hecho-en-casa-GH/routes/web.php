7bbf2a415371d088ff6b1c35a387b06fbc40791d
Joan Pablo <joanpagarf09mail.com>
2025-01-01 23:17:15 -0600

    Prueba de rutas 1

--- hecho-en-casa-GH/routes/web.php
+++ hecho-en-casa-GH/routes/web.php
@@ -1,13 +1,33 @@
 <?php
 
+use App\Models\Elemento;
+use Illuminate\Routing\RouteAction;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', function(){
+    return "Bienvenido a la página de inicio";
 });
-//Comentario
+
+Route::get('/inicio', function(){
+    return "Bienvenido a la página de inicio";
+});
 
 
-//Este comentario está bien chido
 
-//Prueba 123
+//Esta fue la prueba de Jeycson:
+
+/*
+Route::get('/', function () {
+    $elemento = new Elemento;
+    $elemento->nom_elemento = 'Ejemplo';
+    $elemento->precio_e = 0.0;
+    $elemento->save();
+    return $elemento;
+});
+
+Route::get('prueba', function(){
+    
+});
+
+*/
