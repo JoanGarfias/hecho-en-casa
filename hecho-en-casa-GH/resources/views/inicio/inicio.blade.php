2010b71164b81b2251471b0ef0ffebfbcf285c83
Joan Pablo <joanpagarf09mail.com>
2025-01-02 00:54:36 -0600

    Correción de rutas resources

--- hecho-en-casa-GH/resources/views/inicio/inicio.blade.php
+++ hecho-en-casa-GH/resources/views/inicio/inicio.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - Pastelería</title>
-    <link rel="stylesheet" href="{{ asset('css/inicio/menu.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/inicio/pie.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/inicio/cuerpo.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/inicio/menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/inicio/pie.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/inicio/cuerpo.css') }}">
 </head>
 <body>
 
@@ -21,12 +21,12 @@
                     <li><a href="#">Catálogo</a></li>
                 </ul>
                 <div class="logo">
-                    <img src="{{ asset('img/inicio/logo.png') }}" alt="Logo">
+                    <img src="{{ asset('resources/img/inicio/logo.png') }}" alt="Logo">
                 </div>
                 <ul class="menu-derecho">
                     <li><a href="#">Conócenos</a></li>
                     <li><a href="#">Buscar pedido</a></li>
-                    <li><a href="#"><img src="{{ asset('img/inicio/usuario.png') }}" alt="Usuario"></a></li>
+                    <li><a href="#"><img src="{{ asset('resources/img/inicio/usuario.png') }}" alt="Usuario"></a></li>
                 </ul>
             </nav>
         </div>
@@ -35,7 +35,7 @@
     <!-- Banner -->
 
     <section class="banner">
-        <img src="{{ asset('img/inicio/Banner.png') }}" alt="Banner de pasteles">
+        <img src="{{ asset('resources/img/inicio/Banner.png') }}" alt="Banner de pasteles">
     </section>
     <div class="registro-banner">
         ¿No tienes una cuenta? ¡Regístrate y gana increíbles premios!
@@ -48,14 +48,14 @@
         <div class="contenedor">
             <div class="seccion temporada">
                 <h3>TEMPORADA</h3>
-		<img src="{{ asset('img/inicio/temporada.png') }}" alt="Pasteles de temporada">
+		<img src="{{ asset('resources/img/inicio/temporada.png') }}" alt="Pasteles de temporada">
             </div>
             <div class="seccion pasteles">
                 <h3>PASTELES</h3>
                 <div class="contenedor-flex">
-		    <img src="{{ asset('img/inicio/pasteles.png') }}" alt="Pasteles personalizados">
+		    <img src="{{ asset('resources/img/inicio/pasteles.png') }}" alt="Pasteles personalizados">
                     <button class="personaliza">
-			<img src="{{ asset('img/inicio/varita.png') }}" alt="Personaliza tu pastel">
+			<img src="{{ asset('resources/img/inicio/varita.png') }}" alt="Personaliza tu pastel">
                         Personaliza tu pastel
                     </button>
                 </div>
@@ -65,7 +65,7 @@
 
     <!-- Pie de página -->
     <div class="piePa">
-        <img src="{{ asset('img/inicio/PiePag.png') }}" alt="Pie de página">
+        <img src="{{ asset('resources/img/inicio/PiePag.png') }}" alt="Pie de página">
     </div>
 </body>
 </html>
