fb9eb027ef1dc266cae4bc7ba727867aeaf7633e
Joan Pablo <joanpagarf09mail.com>
2025-01-02 01:28:44 -0600

    Cambio de ruta

--- hecho-en-casa-GH/resources/views/inicio/inicio.blade.php
+++ hecho-en-casa-GH/resources/views/inicio/inicio.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - Pastelería</title>
-    @vite(['/css/inicio/menu.css', '/css/inicio/pie.css', '/css/inicio/cuerpo.css'])
+    <!-- @vite(['/css/inicio/menu.css', '/css/inicio/pie.css', '/css/inicio/cuerpo.css']) -->
+    <link rel="stylesheet" href="{{ asset('css/inicio/menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/inicio/pie.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/inicio/cuerpo.css') }}">
+    
 </head>
 <body>
 
