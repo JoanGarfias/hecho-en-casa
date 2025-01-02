d0f4104fae07c452ef6901ca2b33954906a369e7
Joan Pablo <joanpagarf09mail.com>
2025-01-02 00:34:17 -0600

    Prueba de primer vista conectada con Laravel

--- hecho-en-casa-GH/app/Http/Controllers/ControladorInicio.php
+++ hecho-en-casa-GH/app/Http/Controllers/ControladorInicio.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ControladorInicio extends Controller
 {
     public function index(){
-        return "Bienvenido a Hecho en Casa";
+        return view('inicio.inicio');
     }
 }
