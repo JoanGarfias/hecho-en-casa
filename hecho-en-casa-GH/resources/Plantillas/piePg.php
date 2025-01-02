ea2b294c4f1311a5f480602a1ba3a1bb8099f5b8
TheDaniel27 <angeldanieljimenez321@gmail.com>
2025-01-02 11:08:51 -0600

    eso no sirve

--- hecho-en-casa-GH/resources/Plantillas/piePg.php
+++
@@ -1,6 +0,0 @@
-
-<link rel="stylesheet" href="/ProyectoINGS/Pagina/css/pie.css"> <!-- Ruta absoluta -->
-
-    <div class="piePa">
-        <img src="/ProyectoINGS/Pagina/img/piePag.png" alt="Pie de página">
-    </div>
