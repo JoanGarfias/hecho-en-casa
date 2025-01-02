9874502c95f3b8fa7c12f31547a833fe2d49a988
Juan <jemmanuelbn2@gmail.com>
2025-01-02 13:36:39 -0600

    Prueba de formulario para la seleccion de tipo de postre y obtencion de JSON con los postres tipo pop up

--- hecho-en-casa-GH/app/Http/Controllers/ControladorCatalogoEmergente.php
+++ hecho-en-casa-GH/app/Http/Controllers/ControladorCatalogoEmergente.php
@@ -12,6 +12,14 @@ class ControladorCatalogoEmergente extends Controller
                                 ->where('id_tipo_postre', 'temporada')
                                 ->where('disponible', '1')
                                 ->get();
-        return response()->json($emergentes);
+        $json_emergentes = json_encode($emergentes, JSON_PRETTY_PRINT);
+
+        $popups = Catalogo::select('id_postre', 'imagen')
+                                ->where('id_tipo_postre', 'pop-up')
+                                ->where('stock', '>', 0)
+                                ->get();
+
+        $json_popups = json_encode($popups, JSON_PRETTY_PRINT);
+        return $json_emergentes . "\n" . $json_popups;
     }
 }
