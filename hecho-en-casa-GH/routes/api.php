ddd3700ae21f22e4aab05125367ee7be9402b667
Joan Pablo <113860964+joansk09@users.noreply.github.com>
2025-01-06 23:30:34 -0600

    Actualización de archivos

--- hecho-en-casa-GH/routes/api.php
+++
@@ -1,8 +0,0 @@
-use App\Http\Controllers\ControladorCatalogo;
-use Illuminate\Support\Facades\Route;
-
-Route::get('test', function () {
-    return response()->json(['message' => 'API funcionando']);
-});
-
-Route::get('fijo/categorias', [ControladorCatalogo::class, 'obtenerCategorias']);
