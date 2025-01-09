671b713d6b9981cbf64195ee178e3ab8adb3f14c
dxicode <dxicode@gmail.com>
2025-01-09 10:12:41 -0600

    Actualización:
    Iniciar-sesion.blade.php

--- hecho-en-casa-GH/resources/views/iniciar-sesion.blade.php
+++ hecho-en-casa-GH/resources/views/iniciar-sesion.blade.php
@@ -2,35 +2,50 @@
 <title>Iniciar sesión</title>
 <x-menu />    
 
+<div class="flexi">
 <div class = "contenedor">   
     
     <form action="{{route('login.post')}}" method="POST" id="inicioSesion">
         @csrf
         <h2>Iniciar sesión</h2>
         <label for="email">Correo: </label>
-        <input type="email" id = "email" name = "correo_electronico" required> 
-        <div class="mensaje">
-        <p id="errorEmail" class="error"></p>
-        <p id="bienEmail" class="bien"></p>
+        <input type="email" id = "email" name = "email" onfocus="borrarParrafo('mensajeEmail')" required> 
+        <div class="mensajito">
+            <p id="mensajeEmail" class="bien"></p>
         </div>
-
         <br>
-        <label for="password">Contraseña: </label>
-        <input type="password" id = "pass" name ="contraseña"> 
-        <div class="mensaje">
-        <p id="errorPass" class="error"></p>
-        <p id="bienPass" class="bien"></p>
+        <div class="alineando">
+            <label for="password">Contraseña: </label>
+            <div class="campo-contrasena">
+                <input type="password" id="password" name="password"  onfocus="borrarParrafo('mensajePass')" required>
+                <i class="fi fi-rs-crossed-eye visibility" onclick="visibility('password', this)"></i>
+            </div>
         </div>
-        <div>
-            <button class="botoncito" type="submit" name="action" value="recuperar" id="olvidadizo">Olvide mi contraseña</button>
-            <button class="botoncito" type="submit" name="action" value="register">Registrarme</button>
-            <button class="botoncito" type="submit" name="action" value="login" onclick="validateForm()">Continuar</button>
+        <div class="mensajito">
+            <p id="mensajePass" class="bien"></p>
         </div>
-    </form>
+    <div>
+        <button class ="botoncito" id="olvidadizo" type="submit" name="action" value="recuperar">Olvidé mi contraseña</button>
+        <br><br>
+        <button class="botoncito" type="submit" name="action" value="register">Registrarme</button>
+        <button class="botoncito" type="submit" name="action" value="login" >Continuar</button>
+    </div>
+</form>
 </div>
+
+    <div class="fondo-emergente" id="fondoEmergente">
+        <div class="emergente">    
+            <p class="mensajeEmergente">Se ha enviado un código de seguridad a tu correo.</p>
+            <p class="mensajeEmergente"></p>
+            <button id="aceptar" class="aceptando">✔</button>
+        </div>
+    </div>
 </div>
+
 <x-pie/>
 
 <script src="{{ asset('js/iniciando.js') }}"></script>
 <!--Para la animación del logo de usuario-->
 <script src="{{ asset('js/icono.js') }}" defer></script>
+<!--Para borrar el parrafo al hacer click al input-->
+<script src="{{ asset('js/borrandoParrafo.js') }}" defer></script>
