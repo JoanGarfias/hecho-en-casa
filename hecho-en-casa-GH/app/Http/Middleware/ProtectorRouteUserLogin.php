f70895d39c851e5dd7fc0efc7681bdbdf4e39f84
Jeovani Pacheco <regirabanes@gmail.com>
2025-01-10 23:23:46 -0600

    Update ProtectorRouteUserLogin.php

--- hecho-en-casa-GH/app/Http/Middleware/ProtectorRouteUserLogin.php
+++ hecho-en-casa-GH/app/Http/Middleware/ProtectorRouteUserLogin.php
@@ -6,12 +6,28 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use App\Models\usuario;
 
 class ProtectorRouteUserLogin
 {
     public function handle(Request $request, Closure $next)
     {
-        //Todavia falta el codigo xd
+        //$rutaActual = $request->route()->getName();
+        $sessionToken = $request->cookie('session_token');
+        $usuario = usuario::where('token_sesion', $sessionToken)->first();
+
+        if ($sessionToken && $usuario) { 
+            if (request()->routeIs('cerrarsesion.get')){
+                $usuario->update(['token_sesion' => null]);
+                return redirect()->route('inicio.get')->with('info', 'Sesión cerrada exitosamente.');
+            }
+            //dd("No estas permitido entrar por que ya estas logueado");
+            if(request()->routeIs('login.get') || request()->routeIs('login.post') || request()->routeIs('registrar.get') || request()->routeIs('registrar.post')){
+                //return redirect()->route('inicio.get')->with('error', 'No estas permitido entrar por que ya estas logueado');
+                return redirect()->back()->with('error', 'No estás permitido entrar porque ya estás logueado.');
+            }
+        }
+        //dd("No estas logueado");
         return $next($request);
     }
 }
