796269e57f5ad9f5c798ff8830e97cf7e41c7e1e
TheDaniel27 <angeldanieljimenez321@gmail.com>
2025-01-11 00:32:55 -0600

    agregacion del catalogo de pay

---
+++ hecho-en-casa-GH/resources/views/catalogoPay.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="catalogoFijo.css">
+  <title>Pay de Plátano</title>
+</head>
+<body>
+  <div class="title">PAYS</div>
+
+  <div class="main-container">
+    <h2>Plátano</h2>
+    <img src="pay.png" alt="Pay de plátano">
+    <div class="outer-circle">
+      <div class="price-circle">
+        <span>12 px:</span>
+        <span>$650</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="description-container">
+    <span>Base galleta de vainilla, capas de plátano con dulce de leche, queso crema, nuez y más dulce de leche</span>
+    <img src="bolsa.png" alt="Bolsa de compras">
+  </div>
+</body>
+</html>
