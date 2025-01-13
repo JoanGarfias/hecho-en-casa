3aaae11353ab4788fe90955fa2e02ace9d252176
TheDaniel27 <angeldanieljimenez321@gmail.com>
2025-01-13 02:34:16 -0600

    creacion del cokie cake y agregacion de su imagen

--- hecho-en-casa-GH/resources/views/catalogo.blade.php
+++ hecho-en-casa-GH/resources/views/catalogo.blade.php
@@ -4,39 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/menuCatalogoFijo.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/catalogoFijo.css') }}">
-    <title>Pay de Plátano</title>
+    <link rel="stylesheet" href="{{ asset('css/galleta.css') }}">
+    <title>Cokie Cake</title>
 </head>
 <body>
     <x-menu />
-    
+
     <div class="layout">
-        <!-- Incluir el menú -->
         <x-menu-catalogo />
 
-        <!-- Contenido principal -->
         <main class="contenido-principal">
-            <div class="title">PAYS</div>
+            <div class="title">Cokie Cake</div>
 
-            <div class="main-container">
-                <h2>Plátano</h2>
-                <img src="{{ asset('img/pay.png') }}" alt="Pay de plátano">
-                <div class="outer-circle">
-                    <div class="price-circle">
-                        <span>12 px:</span>
-                        <span>$650</span>
+            <div class="grid-container">
+                <!-- Producto 1 -->
+                <div class="main-container">
+                    <h2>Chispas de chocolate</h2>
+                    <img src="{{ asset('img/CookieCake.png') }}" alt="Chispas">
+                    <div class="outer-circle">
+                        <div class="price-circle">
+                            <span>6px:</span>
+                            <span>$360</span>
+                        </div>
+                    </div>
+                    <div class="description-container">
+                        <span>Chispas de chocolate
+                        circular o corazón</span>
+                        <img src="{{ asset('img/bolsa.png') }}" alt="Bolsa de compras">
+                    </div>
+                </div>
+
+                <!-- Producto 2 -->
+                <div class="main-container">
+                    <h2>Red velvet</h2>
+                    <img src="{{ asset('img/CookieCake.png') }}" alt="Chispas de chocolate">
+                    <div class="outer-circle">
+                        <div class="price-circle">
+                            <span>6 px:</span>
+                            <span>$300</span>
+                        </div>
+                    </div>
+                    <div class="description-container">
+                        <span>Red velvet
+                        circular o corazón</span>
+                        <img src="{{ asset('img/bolsa.png') }}" alt="Bolsa de compras">
                     </div>
                 </div>
             </div>
-
-            <div class="description-container">
-                <span>Base galleta de vainilla, capas de plátano con dulce de leche, queso crema, nuez y más dulce de leche</span>
-                <img src="{{ asset('img/bolsa.png') }}" alt="Bolsa de compras">
-            </div>
         </main>
     </div>
+
+    <x-pie />
 </body>
-<x-pie />
-
-
 </html>
