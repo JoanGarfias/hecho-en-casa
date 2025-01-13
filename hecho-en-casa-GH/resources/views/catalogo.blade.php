9116ac2a3965c22c383de12378e3fd90868be1a2
isacar jimenez <isacar300600@gmail.com>
2025-01-13 12:07:32 -0600

    Icono de iniciar sesión
    Arreglo de la vista de calendario

--- hecho-en-casa-GH/resources/views/catalogo.blade.php
+++ hecho-en-casa-GH/resources/views/catalogo.blade.php
@@ -70,5 +70,8 @@
     </div>
 
     <x-pie />
+    <script src="{{asset ('js/despliegue-menu.js')}}" defer> </script>
+    <script src="{{ asset('js/icono.js') }}" defer></script>
 </body>
+
 </html>
