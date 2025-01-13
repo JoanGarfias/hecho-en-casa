31c3e4becdc40e37efc1b3944c5fcd4c0997335b
TheDaniel27 <angeldanieljimenez321@gmail.com>
2025-01-13 11:39:59 -0600

    ver el contenido del catalogo

--- hecho-en-casa-GH/resources/views/catalogo.blade.php
+++ hecho-en-casa-GH/resources/views/catalogo.blade.php
@@ -69,6 +69,16 @@
         </main>
     </div>
 
+
+    <script>
+        let categorias = @json($categorias);
+        let catalogo = @json($catalogo);
+
+        console.log(categorias);
+        console.log(catalogo);
+
+    </script>
+
     <x-pie />
 </body>
 </html>
