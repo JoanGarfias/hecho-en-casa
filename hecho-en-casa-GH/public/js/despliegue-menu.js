fdd33faa5d441915025a1282eb5696d99c197a7f
isacar jimenez <isacar300600@gmail.com>
2025-01-14 16:21:21 -0600

    Despliegue de menu
    Parte izquierda del menu, se cierra al dar clic a otro lugar fuera del menú.

--- hecho-en-casa-GH/public/js/despliegue-menu.js
+++ hecho-en-casa-GH/public/js/despliegue-menu.js
@@ -1,20 +1,22 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const menuIzquierdo = document.querySelector('.menu-izquierdo');
     const menuDerecho = document.querySelector('.menu-derecho');
-    
 
-    // Mostrar/ocultar menú desplegable al hacer clic en el ícono
-    hamburgerMenu.addEventListener('click', function () {
+    // Mostrar/ocultar menús desplegables al hacer clic en el ícono
+    hamburgerMenu.addEventListener('click', function (e) {
+        e.preventDefault(); // Evita recargar la página al hacer clic
         menuIzquierdo.classList.add('show');
-        
+       // menuDerecho.classList.add('show');
     });
 
-    // Cerrar el menú desplegable al hacer clic fuera de él
+    // Cerrar los menús desplegables al hacer clic fuera de ellos
     document.addEventListener('click', function (event) {
-        if (!hamburgerMenu.contains(event.target) && !menuIzquierdo.contains(event.target)) {
+        if (!hamburgerMenu.contains(event.target) && 
+            !menuIzquierdo.contains(event.target) && 
+            !menuDerecho.contains(event.target)) {
             menuIzquierdo.classList.remove('show');
+            menuDerecho.classList.remove('show');
         }
     });
 });
