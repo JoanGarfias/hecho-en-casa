adb64895a7c8ad7c240a0bc1eb783bf5fc88adb1
Jeycson <jeycsonlopez@gmail.com>
2025-01-16 17:05:24 -0600

    Funcionalidad de ajuste de costos en detalles fijo

--- hecho-en-casa-GH/app/Http/Controllers/ControladorCatalogo.php
+++ hecho-en-casa-GH/app/Http/Controllers/ControladorCatalogo.php
@@ -197,7 +197,6 @@ class ControladorCatalogo extends Controller
                 }
 
                 session([
-                    'id_postre' => $postre,
                     'porciones_dia' => $porciones_dia,
                     'cantidad_minima' => $cantidad_minima,
                 ]);
@@ -255,16 +254,11 @@ class ControladorCatalogo extends Controller
         session()->put('proceso_compra', $request->route()->getName());
         /* ENLAZADOR : NO TOCAR O JOAN TE MANDA A LA LUNA */
 
-        session([
-            'fecha' => session("fecha_entrega"),
-            'hora_entrega' => session("hora_entrega"),
-        ]);
-
         //COMO SON DATOS DIRECTOS NO ES NECESARIO ESTO
         //if (!session('postre') || !session('fecha')) {
         //    return redirect()->route('seleccionarFecha')->with('error', 'No se ha seleccionado un postre o fecha.');
         //}
-        
+                                                                //session('postre')
         $postre = Catalogo::where('id_postre', session('id_postre'))->first();
         if ($postre) {
             session([
@@ -280,7 +274,7 @@ class ControladorCatalogo extends Controller
             }
 
             //$listaunidad = PostreFijoUnidad::where('id_pf', $postre->id_postre)->get();
-            $listaunidad = PostreFijoUnidad::where('id_pf', $postre->id_postre)->pluck('id_um'); // Obtener solo la columna 'id_um'
+            /*$listaunidad = PostreFijoUnidad::where('id_pf', $postre->id_postre)->pluck('id_um'); // Obtener solo la columna 'id_um'
             if ($listaunidad->isNotEmpty()) {
                 $unidades = []; 
                 foreach ($listaunidad as $id_um) {  // Ahora recorro la lista de 'id_um'
@@ -289,11 +283,30 @@ class ControladorCatalogo extends Controller
                     if ($nombreunidad) {
                         $unidades[] = [
                             'nombreunidad' => $nombreunidad->nombre_unidad,  // 'nombre_unidad' de la tabla 'UnidadMedida'
-                            'cantidadporciones' => $nombreunidad->cantidad,  // 'cantidad' está en 'UnidadMedida'
+                            'cantidadporciones' => $nombreunidad->cantidad, 
                         ];
                     }
-                }
+                }*/
+            $listaunidad = PostreFijoUnidad::where('id_pf', $postre->id_postre)->pluck('id_um'); // Obtener solo la columna 'id_um'
 
+            if ($listaunidad->isNotEmpty()) {
+                    $unidades = []; 
+                    foreach ($listaunidad as $id_um) {  // Ahora recorro la lista de 'id_um'
+                        $nombreunidad = UnidadMedida::where('id_um', $id_um)->first();  //'UnidadMedida' usando 'id_um'
+                        
+                        if ($nombreunidad) {
+                            // Obtener el precio directamente de PostreFijoUnidad utilizando el id_um
+                            $precio = PostreFijoUnidad::where('id_um', $id_um)->where('id_pf', $postre->id_postre)->first(); 
+                            
+                            if ($precio) {
+                                $unidades[] = [
+                                    'nombreunidad' => $nombreunidad->nombre_unidad,  // 'nombre_unidad' de la tabla 'UnidadMedida'
+                                    'cantidadporciones' => $nombreunidad->cantidad,  // 'cantidad' de 'UnidadMedida'
+                                    'precio' => $precio->precio_um,  // 'precio_um' de 'PostreFijoUnidad'
+                                ];
+                            }
+                        }
+                    }
                 session(['lista_unidad' => $unidades]);  
             } else {
                 session(['lista_unidad' => 'No encontrado']); 
@@ -304,16 +317,24 @@ class ControladorCatalogo extends Controller
             $atributosSesion = [];
 
             foreach ($tiposAtributo as $tipo) {
-                $atributos = $personalizaciones->where('id_tipo_atributo', $tipo->idtipo_atributo)->pluck('nom_atributo')->toArray();
+                //$atributos = $personalizaciones->where('id_tipo_atributo', $tipo->idtipo_atributo)->pluck('nom_atributo')->toArray();
+                $atributos = $personalizaciones
+                ->where('id_tipo_atributo', $tipo->idtipo_atributo)
+                ->map(function ($item) {
+                    return [
+                        'nom_atributo' => $item->nom_atributo,
+                        'precio_a' => $item->precio_a,
+                    ];
+                })->toArray();
+
                 if (!empty($atributos)) {
                     $atributosSesion[$tipo->nombre_atributo] = $atributos;
                 }
             }
 
             session(['atributosSesion' => $atributosSesion]);
-            
         } else {
-            return redirect()->route('fijo.catalogo.get')->with('error', 'Postre no encontrado.');
+            return redirect()->route('seleccionarFecha')->with('error', 'Postre no encontrado.');
         }
 
         $fecha = session('fecha');
@@ -327,6 +348,7 @@ class ControladorCatalogo extends Controller
     }
 
 
+
     public function seleccionarDetalles(Request $request){
         
         /* ENLAZADOR : NO TOCAR O JOAN TE MANDA A LA LUNA */
@@ -336,7 +358,7 @@ class ControladorCatalogo extends Controller
         /* ENLAZADOR : NO TOCAR O JOAN TE MANDA A LA LUNA */
 
 
-        $id_postre = session('postre');
+        $id_postre = session('id_postre');
         $postre = Catalogo::where('id_postre', $id_postre)
                             ->first();
         $costo = intval($request->input('costo'));
