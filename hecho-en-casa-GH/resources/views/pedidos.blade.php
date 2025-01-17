b9155952411bdc47bd17d9d8f7da39019be207df
Jeycson <jeycsonlopez@gmail.com>
2025-01-16 21:11:35 -0600

    Ruta para pedido en sucursal lista

--- hecho-en-casa-GH/resources/views/pedidos.blade.php
+++ hecho-en-casa-GH/resources/views/pedidos.blade.php
@@ -11,6 +11,7 @@
     
     <div class = "contenedor"><!-- café-->
         <form id="formularioPedidos" action="{{route('fijo.detallesPedido.post')}}" method="POST">
+            @csrf
             <div class="dosColumnas">
                 <div class="columna">
                     <div class="fila">
@@ -57,7 +58,7 @@
                         <label for="cantidad">Cantidad:</label>
                         <div class="cantidad-wrapper">
                             
-                            <input type="text" id="cantidad" name="cantidad" min="{{session('cantidad_minima')}}" value="{{session('cantidad_minima')}}">
+                            <input type="text" id="cantidad" name="cantidad" min="1" value="1">
                             <div class="boton-wrapper">
                                 <button type="button" class="flechitas incrementar">🔺</button>
                                 <button type="button" class="flechitas decrementar">🔻</button>
