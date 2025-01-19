b3f1e072ea4a96b1b5f6d3de4d17102d3c8e1499
Jeovani Pacheco <regirabanes@gmail.com>
2025-01-19 13:37:20 -0600

    Update pedidos.blade.php

--- hecho-en-casa-GH/resources/views/pedidos.blade.php
+++ hecho-en-casa-GH/resources/views/pedidos.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset('css/mensajeErrorE.css') }}">
 <link rel="stylesheet" href="{{ asset('css/pedidos.css') }}">
 <link rel="stylesheet" href="{{ asset('css/pedidosTempPop.css') }}">
- 
+
     <title>Pedidos</title>
     <x-menu />
   
@@ -109,10 +109,12 @@
                         <br>
                         <p class="nota">NOTA: El costo es aproximado, el precio final puede variar según su ubicación.</p>
                         <p>Porciones restantes del día: <span id="porcionesRestantes">{{session('porciones')}}</span></p> <br>
-                        <p class="porciones-a-pedir">Porciones a pedir: </p>
+                        <p name="porciones-a-pedir" class="porciones-a-pedir">Porciones a pedir: </p>
                     </div>
                 </div>
             </div>
+            <input type="hidden" id="hiddenCosto" name="costot" value="">
+            <input type="hidden" id="hiddenPorciones" name="porcionest" value="">
             <div class="arrows">
                 <button id="prev" class="arrow botonPr" >⬅</button>
                 <button id="next" class="arrow botonPr">➡</button>
@@ -185,7 +187,11 @@
                 document.querySelector(".flechitas.incrementar").disabled = false;
                 document.getElementById("cantidad").disabled = false;
             }
-        
+
+            // Actualizar los valores en los campos ocultos
+            document.getElementById("hiddenCosto").value = total.toFixed(2);
+            document.getElementById("hiddenPorciones").value = porcionesTotales;
+
             let selects = document.querySelectorAll('select');
             selects.forEach(select => {
                 let selectedOption = select.options[select.selectedIndex];
