871ca61520b6501f8006e31544ed79c1307e5e84
Jeycson <jeycsonlopez@gmail.com>
2025-01-19 10:17:19 -0600

    Modularizacion de script para mensaje de error

--- hecho-en-casa-GH/public/js/meses.js
+++ hecho-en-casa-GH/public/js/meses.js
@@ -265,19 +265,3 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
-
-
-function mostrarMensaje(texto) {
-    const mensaje = document.getElementById('mensajeEmergente');
-    mensaje.textContent = texto; // Agregar texto al mensaje
-    mensaje.style.opacity = '1'; // Mostrar el mensaje
-    mensaje.style.visibility = 'visible'; // Asegurarse de que sea visible
-
-    // Ocultar el mensaje después de 3 segundos
-    setTimeout(() => {
-        mensaje.style.opacity = '0'; // Inicia la transición para ocultar
-        setTimeout(() => {
-            mensaje.style.visibility = 'hidden'; // Ocultar completamente
-        }, 500); // Coincidir con el tiempo de transición de opacity
-    }, 3000);
-}
