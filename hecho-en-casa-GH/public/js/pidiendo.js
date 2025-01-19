67f4853dbefd1b020b14442d79fd102ca377d63b
Jeovani Pacheco <regirabanes@gmail.com>
2025-01-18 22:29:39 -0600

    Update pidiendo.js

--- hecho-en-casa-GH/public/js/pidiendo.js
+++ hecho-en-casa-GH/public/js/pidiendo.js
@@ -15,7 +15,7 @@ document.querySelectorAll('.custom-select-options .option').forEach(option => {
 });
 
 //para el movimiento de los botones de incremento y decremento de porciones
-document.querySelector('.incrementar').addEventListener('click', function() {
+/*document.querySelector('.incrementar').addEventListener('click', function() {
     var cantidadInput = document.getElementById('cantidad');
     cantidadInput.value = parseInt(cantidadInput.value) + 1;
 });
@@ -25,7 +25,7 @@ document.querySelector('.decrementar').addEventListener('click', function() {
     if (parseInt(cantidadInput.value) > 1) { // No permitir que sea menor que 1
         cantidadInput.value = parseInt(cantidadInput.value) - 1;
     }
-});
+});*/
 
 const formulario = document.getElementById('formularioPedidos')
 
