e7676d57c91e303ffa48aeba3abc8efad5ac7f4e
isacar jimenez <isacar300600@gmail.com>
2025-01-20 08:48:53 -0600

    Tamaño del menu despegable.
    .

--- hecho-en-casa-GH/public/js/despliegue-menu.js
+++ hecho-en-casa-GH/public/js/despliegue-menu.js
@@ -3,14 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuIzquierdo = document.querySelector('.menu-izquierdo');
     const menuDerecho = document.querySelector('.menu-derecho');
 
-    
-    console.log('En despliegue ')
     // Mostrar/ocultar menús desplegables al hacer clic en el ícono
     hamburgerMenu.addEventListener('click', function (e) {
         e.preventDefault(); // Evita recargar la página al hacer clic
-        menuIzquierdo.classList.add('show');
-       // menuDerecho.classList.add('show');
-       console.log('En despliegue: se seleciono señor ')
+        menuIzquierdo.style.display =
+            menuIzquierdo.style.display === 'block' ? 'none' : 'block';
+        //menuDerecho.style.display =
+           // menuDerecho.style.display === 'block' ? 'none' : 'block';
     });
 
     // Cerrar los menús desplegables al hacer clic fuera de ellos
@@ -18,8 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!hamburgerMenu.contains(event.target) && 
             !menuIzquierdo.contains(event.target) && 
             !menuDerecho.contains(event.target)) {
-            menuIzquierdo.classList.remove('show');
-            menuDerecho.classList.remove('show');
+            menuIzquierdo.style.display = 'none';
+            menuDerecho.style.display = 'none';
         }
     });
 });
--- hecho-en-casa-GH/resources/views/components/menu.blade.php
+++ hecho-en-casa-GH/resources/views/components/menu.blade.php
@@ -39,6 +39,7 @@
 </header>
 <!--Para la animación del logo de usuario-->
 <script src="{{ asset('js/despliegue-menu.js') }}" defer></script>
+
 <script src="{{ asset('js/icono.js') }}" defer></script>
 
 <script>
