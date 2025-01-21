5fd4821dcbce93e3cff04e75d88f16d11f4c72a9
Jeremy0R <jeremyositoxs@gmail.com>
2025-01-21 17:34:06 -0600

    Resumen de pedido blade
    puse {{$descripcion}} despues del readonly

--- hecho-en-casa-GH/resources/views/ResumenPedioP.blade.php
+++ hecho-en-casa-GH/resources/views/ResumenPedioP.blade.php
@@ -61,7 +61,7 @@
                    <input type="text" id="link_referencia" value="{{$link}}" readonly>
 
                    <label for="descripcion">Descripción detallada:</label>
-                   <textarea name="descripcion" id="descripcion" value="{{$descripcion}}" readonly> </textarea>
+                   <textarea name="descripcion" id="descripcion" readonly> {{$descripcion}} </textarea>
                    
                    <label for="costo_aproximado">Costo Apróximado:</label>
                    <input type="text" id="costo_aproximado" value="${{$costo}}" readonly>
