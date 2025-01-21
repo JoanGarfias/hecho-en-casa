d756214515f69ed61d64bc57acaddd7b8bae4fc8
Joan Pablo <113860964+joansk09@users.noreply.github.com>
2025-01-20 19:31:42 -0600

    Pruebas de logeo

--- hecho-en-casa-GH/tests/Unit/PruebaLogeo.php
+++ hecho-en-casa-GH/tests/Unit/PruebaLogeo.php
@@ -9,7 +9,7 @@ class PruebaLogeo extends TestCase
     public function test_cargar_login()
     {
         // Realiza una solicitud GET a la página de inicio
-        $response = $this->get('/login');
+        $response = $this->get('/');
 
         // Verifica que la respuesta tenga el status 200 (OK)
         $response->assertStatus(200);
@@ -17,19 +17,38 @@ class PruebaLogeo extends TestCase
 
     public function test_ingresar_datos_fallidos()
     {
-        $correo = "joanpagarf09@gmail.com";
-        $password = "Password1";
+        // Datos con credenciales incorrectas
+        $datos_fallidos = [
+            ['email' => 'joanpagarf@gmail.com', 'password' => 'wrongpassword1'],
+            ['email' => 'joanpagarf@gmail.com', 'password' => 'wrongpassword2'],
+            ['email' => 'joanpagarf@gmail.com', 'password' => 'wrongpassword3'],
+            ['email' => 'joanpagarf@gmail.com', 'password' => 'wrongpassword1'],
+        ];
+
+        // Recorre cada combinación de datos fallidos y prueba el login
+        foreach ($datos_fallidos as $datos) {
+            // Realiza la solicitud POST con los datos incorrectos
+            $response = $this->generar_post_logeo($datos['email'], $datos['password']);
+            $response->assertSessionHasErrors(['error']);
+        }
+    }
+
+    public function test_ingresar_datos_correctos()
+    {
+        // Envia una solicitud POST al endpoint de login con credenciales incorrectas
+        $response = $this->generar_post_logeo("joanpagarf09@gmail.com", "Password1!");
     
-        $response = $this->post('/login', [
+        $response->assertSessionHasNoErrors();
+    }
+
+    private function generar_post_logeo($correo, $password)
+    {
+        // Envia una solicitud POST al endpoint de login con las credenciales
+        return $this->post('/login', [
             'action' => 'login',
-            'email' => $correo, 
+            'email' => $correo,
             'password' => $password,
         ]);
-    
-        // Verifica que la respuesta haya sido una redirección o un error de validación
-        $response->assertSessionHasErrors(); 
-    
-        $this->assertGuest();  // Esto asegura que el usuario no esté autenticado
     }
 
 }
