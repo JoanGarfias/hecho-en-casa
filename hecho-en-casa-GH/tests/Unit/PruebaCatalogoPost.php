47a85887cf371431d9ef0a28cea378369016853f
Jeovani Pacheco <regirabanes@gmail.com>
2025-01-20 20:44:18 -0600

    Create PruebaCatalogoPost.php

---
+++ hecho-en-casa-GH/tests/Unit/PruebaCatalogoPost.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class PruebaCatalogoPost extends TestCase
+{
+    public function test_catalogo_post_redirige_a_login_sin_sesion()
+    {
+        $data = [
+            'id_postre' => 1,
+            //'nombre_postre' => 'Pay de platano',
+        ];
+
+        // Realiza la solicitud POST al endpoint del catálogo sin estar autenticado
+        $response = $this->post('fijo/catalogo', $data);
+
+        // Verifica que la respuesta redirija al login
+        $response->assertRedirect(route('login')); 
+    }
+}
