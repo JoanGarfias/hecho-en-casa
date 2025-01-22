af17ea6b59b3269b3df57a12fb3eb34b718db622
Jeremy0R <jeremyositoxs@gmail.com>
2025-01-22 11:46:28 -0600

    se modificó el botón de la pag de inicio

--- hecho-en-casa-GH/resources/views/inicio.blade.php
+++ hecho-en-casa-GH/resources/views/inicio.blade.php
@@ -13,9 +13,8 @@
             <div class="conte_nedor"> 
             <h3>TEMPORADA</h3>
             <img src="{{ asset('img/temporada.png') }}" alt="Pasteles de temporada">
-            <button class="personaliza" onclick="window.location.href='{{ route('emergente.catalogo.get') }}'">
-                <img src="{{ asset('img/varita.png') }}" alt="Personaliza tu pastel">
-                Aprovecha que se acaban
+            <button class="emergente" onclick="window.location.href='{{ route('emergente.catalogo.get') }}'">
+                Ir a Pop-Up
             </button>
             </div>
         </div>
