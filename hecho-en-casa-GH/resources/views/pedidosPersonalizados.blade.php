1d31c0220e265b683d162e9353ab564f8f5cabbc
marianaPT <mptxd9060@gmail.com>
2025-01-29 19:34:50 -0600

    Arreglando una validación (NO BORRAR)
    Ya no se envía si el campo de otros está vacío.

--- hecho-en-casa-GH/resources/views/pedidosPersonalizados.blade.php
+++ hecho-en-casa-GH/resources/views/pedidosPersonalizados.blade.php
@@ -128,7 +128,7 @@
                                 <input type="radio" name="tematica" value="Otro" id="otrosRadio">
                                 <p class="blanca"> Otro</p>
                                 <div id="campoOtros" style="display: none;">
-                                    <input type="text" id="otrosTexto" name="otrosTexto" class="paraOtros" value="">
+                                    <input type="text" id="otrosTexto" name="otrosTexto" class="paraOtros">
                                 </div>
                             </label>
                         </div>
